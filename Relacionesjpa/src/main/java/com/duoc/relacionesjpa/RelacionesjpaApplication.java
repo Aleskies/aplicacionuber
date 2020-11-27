0750c54de46c740968bcc6ff1bc8bc3ed068ac43
Alexis Aravena <geo.mate2010@gmail.com>
2020-11-26 21:50:03 -0300

    1 Inicio de Proyecto "uber"

---
+++ Relacionesjpa/src/main/java/com/duoc/relacionesjpa/RelacionesjpaApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.relacionesjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelacionesjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelacionesjpaApplication.class, args);
+	}
+
+}
